45d010d7c4851ae2fd60b1297f3faf7b1a9950ac
kirillmaksimov <kirillmaksimov@inbox.ru>
2020-03-03 21:03:33 +0300

    Make tic-tac-toe game for a company to be hired

---
+++ README.md
@@ -0,0 +1,42 @@
+# Project Tic-tac-toe
+
+This was a test task for one company, tic-tac-toe game (min size of board 5x5)
+
+## Getting Started
+
+Unzip the project
+
+### Prerequisites
+
+You need to be installed:
+
+```
+nodejs and npm
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```
+yarn install || npm install
+```
+
+yarn start || npm start
+
+```
+open localhost: 3000
+```
+
+
+## Built With
+
+* [React](https://reactjs.org/) - The web framework used
+* [npm](https://nodejs.org/en/) - Dependency Management
+* [Redux](https://redux.js.org/) - State manager
+
+## Authors
+
+* **Kirill Maksimov**
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
+import { configureStore } from './store/configureStore';
+import './index.styles.css';
+import App from './containers/app/app.container';
+import registerServiceWorker from './registerServiceWorker';
+
+const history = createHistory();
+const initialState = {};
+export const store = configureStore(initialState, history);
+
+ReactDOM.render(
+	<Provider store = { store }>
+		<Router history = { history }>
+			<App />
+		</Router>
+	</Provider>, document.getElementById('root'));
+registerServiceWorker();
